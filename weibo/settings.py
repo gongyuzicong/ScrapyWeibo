614012c1abfcd3897fa61d63f3b1d26f83d66842
Tony <tony@vzii.com>
2018-10-29 22:37:15 +0800

    store the infomation to mongodb

--- weibo/settings.py
+++ weibo/settings.py
@@ -14,6 +14,13 @@ BOT_NAME = 'weibo'
 SPIDER_MODULES = ['weibo.spiders']
 NEWSPIDER_MODULE = 'weibo.spiders'
 
+MONGO_URI = 'localhost'
+
+MONGO_DATABASE = 'weibo'
+
+RETRY_HTTP_CODES = [401, 403, 408, 414, 500, 502, 503, 504]
+
+COOKIES = '_T_WM=5e9181dda545c9a05f55c927bb14c4e1; SCF=AqRTZ7bagTQh-4eVCBiVuWwymDb1N3v7G3Htsx5dW9Dx5e8xQ1ekXuOwV35YXuBkY4VP4th4Mkm8sn8nPO24L5o.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWyPScd3E8RlzwWUhT-RpWU5JpX5K-hUgL.Fo2NSozRe0ecS0z2dJLoIf2LxK-L12qLB-2LxKnLB.2LB-zLxK-LBK-L1KBLxKnLBoML1K2LxKML1-2L1hBLxK-L1h-LB-eLxKMLB.-LB.BLxK.LB.zL12-LxKqL1KnL1K-t; SUHB=0oUZzkP08dZ-Gm; SUB=_2AkMsjmYSdcPxrAJWnvodz2jka49H-jyfWw_kAn7oJhMyPRh87gZXqSdutBF-XGoHNtpfQW3SspEs0TePMJH1OLa-; WEIBOCN_FROM=1110006030; MLOGIN=1; M_WEIBOCN_PARAMS=lfid%3D102803%26luicode%3D20000174%26uicode%3D20000174'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'weibo (+http://www.yourdomain.com)'
@@ -39,10 +46,15 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2,mt;q=0.2',
+    'Connection': 'keep-alive',
+    'Host': 'm.weibo.cn',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
@@ -52,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'weibo.middlewares.WeiboDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'weibo.middlewares.CookiesMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -64,9 +76,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'weibo.pipelines.WeiboPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'weibo.pipelines.TimePipeline': 300,
+    'weibo.pipelines.WeiboPipeline': 301,
+    'weibo.pipelines.MongoPipeline': 302,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
